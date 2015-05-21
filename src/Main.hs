0144e0eeee91509a6e370e00c69dedf1f5e2304a
detrevid <detrevid@gmail.com>
2015-05-21 14:17:41 +0200

    Fix style

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,20 @@
 module Main where
 
-    import LexFun
-    import ParFun
-    import AbsFun
-    import Interpreter
+import LexFun
+import ParFun
+import AbsFun
+import Interpreter
 
-    import Control.Monad.State
-    import Data.Map as Map
-    import ErrM
-  
-    main = do
-      interact iterpret
-      putStrLn ""
+import Control.Monad.State
+import Data.Map as Map
+import ErrM
 
-    iterpret s =
-      let p = pProgram (myLexer s)
-      in case p of
-        Ok prog -> show $ transProgram prog
-        Bad m -> show m
+main = do
+  interact iterpret
+  putStrLn ""
+
+iterpret s =
+  let p = pProgram (myLexer s)
+  in case p of
+    Ok prog -> show $ transProgram prog
+    Bad m -> show m
