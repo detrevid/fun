053e3afdcf1445c4702cbda5a7393622f92cab5b
detrevid <detrevid@gmail.com>
2015-05-22 21:53:03 +0200

    Add EvalM monad

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Interpreter where
 
 import AbsFun
@@ -8,6 +10,7 @@ import Type
 import qualified Data.Map as Map
 import Debug.Trace
 import Control.Monad.State
+import qualified Control.Monad.Trans.State as StateT
 
 data Value
   = VInt Integer
@@ -20,7 +23,10 @@ data Fun = Fun { args :: [Ident], exp :: Exp, env :: Env } deriving (Eq)
 
 type Result = Err Value
 type Env = Map.Map Ident Value
-type Eval = State Env Result
+type EvalM a = StateT.StateT Env Err a
+type Eval = EvalM Value
+
+internalErrMsg = "Internal error"
 
 emptyEnv :: Env
 emptyEnv = Map.empty
@@ -32,18 +38,13 @@ instance Show Value where
     VFun _ -> show "Unnamed function"
     VNFun (Ident id) _ -> show "Function: " ++ show id
 
-failure :: Show a => a -> Result
-failure x = Bad $ "Undefined case: " ++ show x
-
-success :: Value -> Result
-success x = Ok $ x
-
 transProgram :: Program -> Result
 transProgram x = do
   checkTypes x
   case x of
-    Prog stmts ->
-      fst $ foldl (\x y -> runState (transStmt y) (snd x)) (Ok $ VBool True, emptyEnv) stmts
+    Prog stmts -> do
+      (v, _) <- foldM (\(_, env') y -> runStateT (transStmt y) env')  (VBool True, emptyEnv) stmts
+      return v
 
 transStmt :: Stmt -> Eval
 transStmt x = case x of
@@ -56,30 +57,26 @@ transDecl x = case x of
     oldEnv <- get
     let fun = VNFun id $ Fun args exp oldEnv
     modify (Map.insert id fun)
-    return $ Ok $ fun
+    return $ fun
   DVal id exp -> do
     e1 <- transExp exp
-    case e1 of
-      Ok e1' -> do
-        modify (Map.insert id e1')
-        return e1
-      Bad m -> return $ Bad m
+    modify (Map.insert id e1)
+    return e1
 
 transExp :: Exp -> Eval
 transExp x = case x of
   ELet decl exp2  -> do
     oldEnv <- get
     e1 <- transDecl decl
-    case e1 of
-      Ok e1' -> do
-        result <- transExp exp2
-        put oldEnv
-        return result
-      Bad m -> return $ Bad m
+    result <- transExp exp2
+    put oldEnv
+    return result
   EIf cond exp1 exp2  -> do
-    Ok (VBool c) <- transExp cond
-    if c then transExp exp1
-         else transExp exp2
+    c <- transExp cond
+    case c of
+      VBool b -> if b then transExp exp1
+                      else transExp exp2
+      _       -> fail internalErrMsg
   ELog exp1 logopr exp2  -> evalBinOpExp (transLogOpr logopr) exp1 exp2
   EEq exp1 eqopr exp2  -> evalBinOpExp (transEqOpr eqopr) exp1 exp2
   ERel exp1 relopr exp2  -> evalBinOpExp (transRelOpr relopr) exp1 exp2
@@ -89,54 +86,51 @@ transExp x = case x of
   EVal id -> do
     val <- gets (Map.lookup id)
     case val of
-      Just v -> return $ Ok $ v
-      Nothing -> return $ Bad $ "Undeclared variable: " ++ show id
+      Just v -> return v
+      Nothing -> fail internalErrMsg
   EConst const  -> transConstant const
   ELam args exp -> do
-    env <- get
-    return $ transLam args exp env
+    v <- transLam args exp
+    return $ v
   EApp exp1 exp2  -> transApp exp1 exp2
 
-evalBinOpExp :: (Value -> Value -> Result) -> Exp -> Exp -> Eval
+evalBinOpExp :: (Value -> Value -> Eval) -> Exp -> Exp -> Eval
 evalBinOpExp op exp1 exp2 = do
   e1 <- transExp exp1
   e2 <- transExp exp2
-  case (e1, e2) of
-    (Ok le, Ok re) -> return $ op le re
-    (Bad m, _) -> return $ Bad m
-    (_, Bad m) -> return $ Bad m
+  sol <- op e1 e2
+  return sol
 
 evalNeg :: Exp -> Eval
 evalNeg exp = do
   e <- transExp exp
   case e of
-    Ok (VBool b) -> return $ success $ VBool $ not b
-    Bad m        -> return $ Bad m
-    _            -> return $ Bad $ "Cannot apply negation to the expression: " ++ show exp
+    VBool b -> return $ VBool $ not b
+    _       -> fail internalErrMsg
 
 transConstant :: Constant -> Eval
 transConstant x = case x of
-  CTrue   -> return $ Ok $ VBool True
-  CFalse  -> return $ Ok $ VBool False
-  CInt n  -> return $ Ok $ VInt n
+  CTrue   -> return $ VBool True
+  CFalse  -> return $ VBool False
+  CInt n  -> return $ VInt n
 
 
-transLogOpr :: LogOpr -> Value -> Value -> Result
+transLogOpr :: LogOpr -> Value -> Value -> Eval
 transLogOpr x a b = case (x, a, b) of
-  (OOr, VBool v1, VBool v2)  -> success $ VBool $ v1 || v2
-  (OAnd, VBool v1, VBool v2) -> success $ VBool $ v1 && v2
-  _                          -> failure (x, a, b)
+  (OOr, VBool v1, VBool v2)  -> return $ VBool $ v1 || v2
+  (OAnd, VBool v1, VBool v2) -> return $ VBool $ v1 && v2
+  _                          -> fail internalErrMsg
 
 evalEqOpr :: Eq a => EqOpr -> a -> a -> Bool
 evalEqOpr opr = case opr of
   OEq -> (==)
   ONeq -> (/=)
 
-transEqOpr :: EqOpr -> Value -> Value -> Result
+transEqOpr :: EqOpr -> Value -> Value -> Eval
 transEqOpr opr a b = case (a, b) of
-  (VBool v1, VBool v2)  -> success $ VBool $ (evalEqOpr opr) v1 v2
-  (VInt v1, VInt v2)    -> success $ VBool $ (evalEqOpr opr) v1 v2
-  _                     -> failure (a, b)
+  (VBool v1, VBool v2)  -> return $ VBool $ (evalEqOpr opr) v1 v2
+  (VInt v1, VInt v2)    -> return $ VBool $ (evalEqOpr opr) v1 v2
+  _                     -> fail internalErrMsg
 
 evalRelOpr :: RelOpr -> Integer -> Integer -> Bool
 evalRelOpr opr = case opr of
@@ -145,60 +139,57 @@ evalRelOpr opr = case opr of
   OLeq -> (<=)
   OGeq -> (>=)
 
-transRelOpr :: RelOpr -> Value -> Value -> Result
+transRelOpr :: RelOpr -> Value -> Value -> Eval
 transRelOpr opr a b = case (a, b) of
-  (VInt v1, VInt v2) -> success $ VBool $ (evalRelOpr opr) v1 v2
-  _                  -> failure (a, b)
+  (VInt v1, VInt v2) -> return $ VBool $ (evalRelOpr opr) v1 v2
+  _                  -> fail internalErrMsg
 
 evalAddOpr :: AddOpr -> Integer -> Integer -> Integer
 evalAddOpr opr = case opr of
   OAdd -> (+)
   OSub -> (-)
 
-transAddOpr :: AddOpr -> Value -> Value -> Result
+transAddOpr :: AddOpr -> Value -> Value -> Eval
 transAddOpr opr a b = case (a, b) of
-  (VInt v1, VInt v2) -> success $ VInt $ (evalAddOpr opr) v1 v2
-  _                  -> failure (opr, a, b)
+  (VInt v1, VInt v2) -> return $ VInt $ (evalAddOpr opr) v1 v2
+  _                  -> fail internalErrMsg
 
-transMulOpr :: MulOpr -> Value -> Value -> Result
+transMulOpr :: MulOpr -> Value -> Value -> Eval
 transMulOpr x a b = case (x, a, b) of
-  (OMul, VInt v1, VInt v2) -> success $ VInt $ v1 * v2
+  (OMul, VInt v1, VInt v2) -> return $ VInt $ v1 * v2
   (ODiv, VInt v1, VInt v2) ->
-    if v2 /= 0 then success $ VInt $ v1 `quot` v2
-               else Bad $ "Division by zero"
-  _                       -> failure (x, a, b)
+    if v2 /= 0 then return $ VInt $ v1 `quot` v2
+               else fail "Division by zero"
+  _                       -> fail internalErrMsg
 
-transLam :: [Ident] -> Exp -> Env -> Result
-transLam args exp env = do
-  (success $ VFun $ Fun args exp env)
+transLam :: [Ident] -> Exp -> Eval
+transLam args exp = do
+  env <- get
+  return $ VFun $ Fun args exp env
 
 funApp :: Fun -> Exp -> Eval
 funApp (Fun args exp env) exp2 = do
   oldEnv <- get
   e <- transExp exp2
-  case e of
-    Ok e' -> do
-      put env
-      modify (Map.insert (head args) e')
-      let newArgs = tail args
-      if not $ null newArgs
-        then do
-          newEnv <- get
-          let f = VFun $ Fun newArgs exp newEnv
-          put oldEnv
-          return $ Ok $ f
-        else do
-          result <- transExp exp
-          put oldEnv
-          return $ result
-    Bad m -> return $ Bad m
+  put env
+  modify (Map.insert (head args) e)
+  let newArgs = tail args
+  if not $ null newArgs
+    then do
+      newEnv <- get
+      let f = VFun $ Fun newArgs exp newEnv
+      put oldEnv
+      return f
+    else do
+      result <- transExp exp
+      put oldEnv
+      return result
 
 transApp :: Exp  -> Exp -> Eval
 transApp exp1 exp2 = do
   f <- transExp exp1
   case f of
-    Ok (VFun f') -> funApp f' exp2
-    Ok f'@(VNFun name (Fun args exp3 env)) ->
+    VFun f' -> funApp f' exp2
+    f'@(VNFun name (Fun args exp3 env)) ->
       funApp (Fun args exp3 (Map.insert name f' env)) exp2
-    Bad m -> return $ Bad m
-    _     -> return $ Bad $ "Expression is not a function: " ++ show f
+    _     -> fail internalErrMsg
